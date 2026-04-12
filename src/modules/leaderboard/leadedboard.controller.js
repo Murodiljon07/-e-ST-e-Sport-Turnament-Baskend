c74910b08b8fb5a1272b226b5aea0bb33a035965
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-04-12 16:49:58 +0500

    the project start with clean code

--- src/modules/leaderboard/leadedboard.controller.js
+++ src/modules/leaderboard/leadedboard.controller.js
@@ -1,5 +1,11 @@
+import { getTournamentsService } from "../tournament/tournament.service.js";
+
 export const getLeadBoardTurnaments = async (req, res) => {
-  try {
-  } catch (error) {}
+  const turnaments = await getTournamentsService();
+  const LeadBoardTurnaments = await turnaments.filter(
+    (item) => item.inform === true,
+  );
+
+  res.json(LeadBoardTurnaments);
 };
 export const getLeadBoardPlayers = async (req, res) => {};
